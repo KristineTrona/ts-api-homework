ccf7395df624a7167db36703c639a58544b2f7f2
Kristine <kristinetrona@gmail.com>
2018-09-15 15:37:18 +0200

    all finished

--- src/games/controller.ts
+++ src/games/controller.ts
@@ -19,51 +19,48 @@ const moves = (board1, board2) =>
 @JsonController()
 export default class GameController {
 
-    
-    @Get('/games')
-    async allGames () {
-      const games = await Game.find()
-      return { games } 
-    }
+  @Get('/games')
+  async allGames () {
+    const games = await Game.find()
+    return { games } 
+  }
 
-    @Post('/games')
-    @HttpCode(201)
-    createGame(
-      @BodyParam("name") name: string
-    ) {
-        const newGame = new Game
+  @Post('/games')
+  @HttpCode(201)
+  createGame(
+    @BodyParam("name") name: string
+  ) {
+      const newGame = new Game
+      newGame.name = name
+      newGame.color = colors[Math.floor((Math.random()*colors.length-1)+1)]
+      newGame.board = defaultBoard
 
-        newGame.name = name
-        newGame.color = colors[Math.floor((Math.random()*colors.length-1)+1)]
-        newGame.board = defaultBoard
+    return newGame.save()
+  }
 
-      return newGame.save()
-    }
+  @Put('/games/:id')
+  async updateGame(
+    @Param("id") id: number,
+    @BodyParam("name") name: string,
+    @BodyParam("color") color: string,
+    @BodyParam("board") board: object,
+    @Body() update: Partial<Game>)
+    {     
+      const game = await Game.findOne(id)
 
-    @Put('/games/:id')
-    async updateGame(
-      @Param("id") id: number,
-      @BodyParam("name") name: string,
-      @BodyParam("color") color: string,
-      @BodyParam("board") board: object,
-      @Body() update: Partial<Game>
-    ) {     
-        const game = await Game.findOne(id)
-        if(!game)
-          throw new NotFoundError("Cannot find the game")
+      if(!game)
+        throw new NotFoundError("Cannot find the game")
 
-        else if(color && !colors.includes(color))
-          throw new BadRequestError("Please select a valid color: red, blue, green, yellow or magenta ")
+      else if(color && !colors.includes(color))
+        throw new BadRequestError("Please select a valid color: red, blue, green, yellow or magenta ")
 
-        else if(board && moves(board, game.board)>1)
-          throw new BadRequestError("Make only one move at the time!")
+      else if(board && moves(board, game.board)>1)
+        throw new BadRequestError("Make only one move at the time!")
 
-        game.name = name
-        game.color = color
-        game.board = board
+      game.name = name
+      game.color = color
+      game.board = board
 
-        return Game.merge(game, update).save()
-      }
-    }
-
-
+    return Game.merge(game, update).save()
+  }
+}
